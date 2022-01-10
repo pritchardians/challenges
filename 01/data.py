c95c3c9af86eb6e0e151d70cffa3895c98fde981
Ian Pritchard <pritchardians@gmail.com>
2022-01-09 18:29:49 -0600

    create word list

--- 01/data.py
+++ 01/data.py
@@ -3,4 +3,9 @@ DICTIONARY = 'dictionary.txt'
 scrabble_scores = [(1, "E A O I N R T L S U"), (2, "D G"), (3, "B C M P"),
                    (4, "F H V W Y"), (5, "K"), (8, "J X"), (10, "Q Z")]
 LETTER_SCORES = {letter: score for score, letters in scrabble_scores
-                              for letter in letters.split()}
+                 for letter in letters.split()}
+
+with open(DICTIONARY) as f:
+    data = f.readlines()
+    for word in data:
+        print(word)
